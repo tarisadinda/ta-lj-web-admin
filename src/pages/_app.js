d86ba6c7e0347662650eec0fad48cfffc5ed7bdd
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-06 01:04:00 +0700

    add layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(<>
+    <Component {...pageProps} />
+  </>) 
 }
