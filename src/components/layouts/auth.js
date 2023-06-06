873639ad5f92bc30c9c8761f77eef0bb9abe865a
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-06 10:47:19 +0700

    edit height & padding

--- src/components/layouts/auth.js
+++ src/components/layouts/auth.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import Footer from './footer'
+import Footer from '../footer'
 
 export default function LayoutAuth({ children }) {
     return (
       <>
         <style jsx global>{`
             .auth-wrap {
+                height: 100%;
                 position: absolute;
                 top: 50%;
                 left: 50%;
                 transform: translate(-50%, -50%);
+                display: flex;
+                flex-direction: column;
+                padding: 100px 0px 30px 0px;
             }
 
             .main {
