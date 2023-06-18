f1750a865008d449e793441db46d4f2c8c08a797
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-18 21:25:42 +0700

    edit font style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.scss'
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+  },
+})
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return getLayout(<>
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   </>) 
 }
