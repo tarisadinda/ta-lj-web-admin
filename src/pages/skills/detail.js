98903027f6b800cc4a482d4e716168ce1af787fb
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-26 20:42:17 +0700

    add dialog

--- src/pages/skills/detail.js
+++ src/pages/skills/detail.js
@@ -2,9 +2,39 @@ import cn from 'classnames'
 import LayoutMain from "@/components/layouts/main"
 import styles from '@/styles/pages/skills/DetailSkill.module.scss'
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile'
-import { Card } from '@mui/material'
+import { Card, DialogContent } from '@mui/material'
+import CustomDialog from '@/components/dialog'
+import React from 'react'
+import CustomDropdown from '@/components/custom-dropdown'
+
+const statusData = [
+    {
+        label: 'Menunggu Konfirm',
+        value: '1'
+    },
+    {
+        label: 'Proses Sertifikasi',
+        value: '2'
+    },
+    {
+        label: 'Selesai',
+        value: '3'
+    },
+    {
+        label: 'Gagal',
+        value: '4'
+    },
+]
 
 export default function DetailSkill() {
+    const [openDialog, setOpenDialog] = React.useState(false)
+    
+    //menunggu konfirm, proses sertifikasi, selesai, gagal
+    const handleDialog = () => {
+        setOpenDialog(!openDialog)
+        console.log('klik')
+    }
+
     return(<>
         <p className={styles.name}><b>Christian Wijaya</b></p>
         <h5 className={styles.silverText}>Sidoarjo, Jawa Timur</h5>
@@ -41,12 +71,24 @@ export default function DetailSkill() {
             </button>
         </div>
         <Card variant="outlined" className={styles.cardStatus}>
-            <p className='mb-0'><b>Status Pengajuan</b></p>
-            <p  className='mb-0'>Menunggu konfirmasi</p>
+            <div className='d-inline-flex gap-3'>
+                <p className='mb-0'><b>Status Pengajuan</b></p>
+                <p  className='mb-0'>Menunggu konfirmasi</p>
+            </div>
             <div className={styles.updateBtn}>
-                <button className='btn btn-primary blue'><b>Perbarui</b></button>
+                <button className='btn btn-primary blue' onClick={handleDialog}><b>Perbarui</b></button>
             </div>
         </Card>
+        <CustomDialog 
+            open={openDialog} 
+            handleClose={() => setOpenDialog(false)}
+            title='Edit Status Pengajuan'
+        >
+            <DialogContent dividers>
+                <span>Status Pengajuan</span>
+                <CustomDropdown data={statusData} />
+            </DialogContent>
+        </CustomDialog>
     </>)
 }
 
