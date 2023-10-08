80615b590a5ecf735ccbc6acba602d1b0f543316
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-08 17:35:27 +0700

    edit login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import LayoutAuth from "@/components/layouts/auth"
 import styles from "@/styles/pages/Login.module.scss"
-import axios from "axios"
 import { Alert, Snackbar } from '@mui/material'
 import cn from 'classnames'
 import Link from "next/link"
 import { useRouter } from "next/router"
 import React from "react"
-import { getToken, setToken } from "src/utils/token"
+import { setToken } from "src/utils/token"
+import InputIcon from "@/components/common/input-icon"
+import { axiosInstance } from "src/utils/axios"
+import SVGEye from '@/public/icons/eye.svg'
+import SVGEyeClose from '@/public/icons/eye-closed.svg'
 
 export default function Login() {
     let vertical = 'top'
@@ -15,14 +18,18 @@ export default function Login() {
     const LOGIN_API = '/admin/login'
 
     const router = useRouter()
-    const token = getToken()
     const [errorMsg, setErrorMsg] = React.useState('')
+    const [seePassword, setSeePassword] = React.useState(false)
     const [userAccount, setUserAccount] = React.useState({
         username: '',
         password: ''
     })
 
     const [openToast, setOpenToast] = React.useState(false)
+
+    const handleVisibility = () => {
+        setSeePassword(!seePassword)
+    }
 
     const handleChange = (e) => {
         setUserAccount({
@@ -31,7 +38,7 @@ export default function Login() {
         })
     }
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
 
         const formData = {
@@ -39,48 +46,59 @@ export default function Login() {
             password: userAccount.password
         }
 
-        try {
-            const res = await axios({
-                method: 'post',
-                url: process.env.NEXT_PUBLIC_API_URL + LOGIN_API,
-                data: formData,
-                headers: { "Content-Type": "multipart/form-data" },
-            })
-
-            if(res.status === 200) {
+        axiosInstance.post(LOGIN_API, formData)
+        .then((res) => {
+            console.log(res)
+            if(res.data.status === "success") {
                 setToken(res.data.token)
-
                 router.push('/dashboard')
             }
-        } catch (err) {
+        }).catch((err) => {
             if(err) {
                 if( err.response?.status !== 200) {
                     setErrorMsg(err.response.data.message)
                     setOpenToast(true)
                 }
             }
-        }
+        })
     }
 
     return(<>
         <div className={styles.wrapper}>
             <h2 className="d-flex justify-content-center mb-5">Login Admin</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className={styles.formWrap}>
                 <div className={styles.form}>
-                    <div className="mb-3">
-                        <label htmlFor="uusernameame" className="form-label"><b>Username</b></label>
-                        <input type="text" onChange={handleChange} className={cn(styles.inputField, "form-control")} id="username" name="username" placeholder="Masukkan username admin" />
+                    <div className={cn(styles.inputGroup, "mb-3")}>
+                        <label htmlFor="username" className="form-label"><b>Username</b></label>
+                        <input type="text" 
+                            onChange={handleChange} 
+                            className={cn(styles.inputField, "form-control")} 
+                            id="username" 
+                            name="username" 
+                            placeholder="Masukkan username admin" 
+                        />
                     </div>
-                    <div className="mb-3">
+                    <div className={cn(styles.inputGroup, "mb-1")}>
                         <label htmlFor="password" className="form-label"><b>Password</b></label>
-                        <input type="password" onChange={handleChange} className={cn(styles.inputField, "form-control")} id="password" name="password" placeholder="Masukkan password" />
+                        <InputIcon 
+                            placeholder="Masukkan password"
+                            id="password"
+                            name="password"
+                            type={seePassword ? "text" : "password"}
+                            onChange={handleChange}
+                            onClick={handleVisibility}
+                            icon={seePassword ? <SVGEyeClose className={styles.eyeIcon} /> : <SVGEye className={styles.eyeIcon} />}
+                        />
                     </div>
-                    <div className="my-3">
+                    <div className="mb-4">
                         <span className={styles.forgetText}>
                             <Link href='#'>Lupa password?</Link>
                         </span>
                     </div>
                     <button type="submit" className={cn(styles.loginBtn, "btn btn-primary blue")}>Login</button>
+                    <div className="mt-2">
+                        <p className={styles.registText}>Belum punya akun? <Link href="#"><span>Daftar</span></Link></p>
+                    </div>
                 </div>
             </form>
         </div>
