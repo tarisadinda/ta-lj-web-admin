76f8e7e6a7b5307c9d41a0984596fbb7f70c6865
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-13 22:18:33 +0700

    edit props

--- src/components/common/icon-button.js
+++ src/components/common/icon-button.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from '@/styles/components/common/IconButton.module.scss'
 import cn from 'classnames'
 
-export default function IconBtn({ title, className, startIcon = '', endIcon = '' }) {
+export default function IconBtn({ title, onClick, className, startIcon = '', endIcon = '' }) {
     return(<>
-        <button className={cn(styles.iconBtn, className)}>
+        <button onClick={onClick} className={cn(styles.iconBtn, className)}>
             {startIcon !== '' && startIcon}
             <span>{title}</span>
             {endIcon !== '' && endIcon}
--- src/components/common/input-icon.js
+++ src/components/common/input-icon.js
@@ -2,7 +2,7 @@ import styles from '@/styles/components/common/InputIcon.module.scss'
 import cn from 'classnames'
 import { IconButton } from '@mui/material'
 
-export default function InputIcon({ placeholder, id, type, icon, name, onChange, onClick }) {
+export default function InputIcon({ placeholder, id, type, icon, name, onChange, onClick, register }) {
     return(<>
         <div className={styles.wrapper}>
             <input 
@@ -12,6 +12,7 @@ export default function InputIcon({ placeholder, id, type, icon, name, onChange,
                 type={type}
                 className={cn(styles.inputWrap, "form-control")} 
                 onChange={onChange}
+                {...(register && register(name))}
             />
             <div className={styles.iconWrapper}>
                 <IconButton className={styles.btnIcon} onClick={onClick}>
