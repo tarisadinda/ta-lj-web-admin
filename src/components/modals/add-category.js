22884925c48625dd61a903702027ce5a09798bd9
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-14 20:12:05 +0700

    integration

--- src/components/modals/add-category.js
+++ src/components/modals/add-category.js
@@ -2,14 +2,38 @@ import styles from '@/styles/components/modals/AddCategoryModal.module.scss'
 import cn from 'classnames'
 import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
-import { useDispatch, useSelector } from 'react-redux'
-import { openModal, setOpenModal } from 'src/redux/slices/modalSlice'
+import { useDispatch } from 'react-redux'
+import { openModal } from 'src/redux/slices/modalSlice'
+import { axiosInstance } from 'src/utils/axios'
+import { useState } from 'react'
+import { API_ADD_CAT } from 'src/utils/api'
+import { setMessage, setOpenAlert } from 'src/redux/slices/alertSlice'
 
 export default function AddCategoryModal({ open, onClose }) {
-    const tes = useSelector(openModal)
     const dispatch = useDispatch()
 
-    console.log(tes)
+    const [catName, setCatName] = useState('')
+    
+    const saveCategory = (e) => {
+        e.preventDefault()
+
+        const data = {
+            category_name: catName
+        }
+
+        axiosInstance.post(API_ADD_CAT, data)
+        .then((res) => {
+            console.log(res)
+            onClose()
+
+            if(res.status === 200) {
+                dispatch(setOpenAlert(true))
+                dispatch(setMessage('Data berhasil ditambahkan'))
+            }
+        }).catch((err) => {
+        })
+    }
+
     return(<>
         <Dialog open={open} fullWidth={true} maxWidth='xs'>
             <DialogTitle sx={{ padding: '10px 15px' }}>
@@ -21,11 +45,11 @@ export default function AddCategoryModal({ open, onClose }) {
             <DialogContent dividers>
                 <div>
                     <label className={styles.inputLabel}>Nama Kategori</label>
-                    <input type='text' className='form-control' />
+                    <input type='text' className='form-control' name='category_name' onChange={(e) => setCatName(e.target.value)} />
                 </div>
                 <div className={styles.actionBtn}>
                     <button onClick={onClose} className={cn(styles.cancelBtn, 'btn btn-ghost')}>Batal</button>
-                    <button className={cn(styles.saveBtn, 'btn btn-primary blue')}>Simpan</button>
+                    <button onClick={saveCategory} className={cn(styles.saveBtn, 'btn btn-primary blue')}>Simpan</button>
                 </div>
             </DialogContent>
         </Dialog>
