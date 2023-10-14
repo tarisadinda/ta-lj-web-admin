b94e5820c89526dd23827ac9b3b4524f044bbda9
tarisadinda20 <tarisadinda20@gmail.com>
2023-10-14 14:25:23 +0700

    edit format date

--- src/utils/convert-date.js
+++ src/utils/convert-date.js
@@ -7,5 +7,5 @@ export const convertDate = (timestamp) => {
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const seconds = date.getSeconds().toString().padStart(2, '0');
     
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
